23925d171c1eaa6fbbe8fe6f001464a6d80dc3e7
Yana <petenyova2018@gmail.com>
2024-08-06 14:24:54 +0300

    Решение с помощью рекурсии

--- Tasks/task_3/Program.cs
+++ Tasks/task_3/Program.cs
@@ -2,3 +2,35 @@
 // Задача 3: Задайте произвольный массив. Выведете его элементы, начиная
 // с конца. Использовать рекурсию, не использовать циклы.
 // ----------------------------------------------------------------------
+
+int[] InputArray(string prompt)
+{
+    Console.Write(prompt);
+    string input = Console.ReadLine()!;
+    string[] parts = input.Split(' '); // 43 23 61 1 3
+    int[] result = new int[parts.Length];
+
+    for (int i = 0; i < parts.Length; i++)
+    {
+        result[i] = int.Parse(parts[i]);
+    }
+
+    return result;
+}
+
+int[] input3 = InputArray("ARR: ");
+
+void OutputArrayReverse(int[] array, int i)
+{
+    if (i < 0)
+    {
+        return;
+    }
+    
+    Console.Write(array[i] + " ");
+    
+    OutputArrayReverse(array, i - 1);
+}
+
+OutputArrayReverse(input3, input3.Length - 1);
+Console.WriteLine();
