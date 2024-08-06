9faf55b45430f9f080155642b7970eebfabeda87
Yana <petenyova2018@gmail.com>
2024-08-06 14:15:19 +0300

    Решение с помощью рекурсии

--- Tasks/task_1/Program.cs
+++ Tasks/task_1/Program.cs
@@ -3,3 +3,29 @@
 // все натуральные числа в промежутке от M до N. Использовать рекурсию,
 // не использовать циклы.
 // ----------------------------------------------------------------------
+int InputNumber(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine());
+}
+
+int m1 = InputNumber("M: ");
+int n1 = InputNumber("N: ");
+
+int min1 = Math.Min(m1, n1);
+int max1 = Math.Max(m1, n1);
+
+void OutputNumbersBetween(int min, int max)
+{
+    Console.Write(min + " ");
+
+    if (min == max)
+    {
+        return;
+    }
+
+    OutputNumbersBetween(min + 1, max);
+}
+
+OutputNumbersBetween(min1, max1);
+Console.WriteLine();
