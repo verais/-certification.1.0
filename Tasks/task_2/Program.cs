07e474d7903b6e94b7ef16b5d9a7d1c922e2eee4
Yana <petenyova2018@gmail.com>
2024-08-06 14:21:20 +0300

    Решение с помошью функции Аккермана

--- Tasks/task_2/Program.cs
+++ Tasks/task_2/Program.cs
@@ -2,3 +2,23 @@
 // Задача 2: Напишите программу вычисления функции Аккермана с помощью
 // рекурсии. Даны два неотрицательных числа m и n.
 // ----------------------------------------------------------------------
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+
+    if (m > 0 && n == 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+
+    return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int m2 = InputNumber("M: ");
+int n2 = InputNumber("N: ");
+
+int result2 = Akkerman(m2, n2);
+Console.WriteLine(result2);
